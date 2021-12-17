b10e648d7bd8f9af37adaf3de6429c8ae4cfe14c
Adam Kudłacik <adam.kud@wp.pl>
2021-12-17 04:00:55 +0100

    add colors and loops

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Card = () => {
+  return (
+    <div>
+      <h2>Card example</h2>
+      <div className="card">
+        <h1 className="card-title">This is a card title</h1>
+        <p className="card-body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat optio
+          at veniam beatae! Iure neque quod non ut dicta cupiditate repellat
+          assumenda error, reiciendis voluptas officia
+          <a href="#">link</a> molestias, tenetur molestiae? Animi.
+        </p>
+      </div>
+    </div>
+  );
+};
